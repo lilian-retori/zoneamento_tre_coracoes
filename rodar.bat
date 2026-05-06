500e273079edb376aeed203015217cd04dc8f135
Lilian <lilian@example.com>
2026-05-06 05:21:52 -0300

    feat: primeira versão do zoneamento urbano

---
+++ rodar.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Users\lilian retori\AppData\Local\miniconda3\Scripts\activate.bat"
+cd /d "Z:\11- FISCAIS\4 - Lilian\Provas\PROJETOS\Arrecadação\Zoneamento"
+streamlit run app.py
+pause
